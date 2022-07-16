990fa6b78ee5b3258fa67049569576f20c5c1431
favas <favas@uitoux.in>
2022-07-16 12:02:08 +0530

    added second activity by favas

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.test1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,7 +22,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Toast.makeText(getApplicationContext(),"Bismillahi",Toast.LENGTH_LONG).show();
+                Intent intent= new Intent( getApplicationContext(),SecondActivity.class);
+                startActivity(intent);
+
+               // Toast.makeText(getApplicationContext(),"Bismillahi",Toast.LENGTH_LONG).show();
             }
         });
 
